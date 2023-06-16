ff5606ee8f0fddc761352e88e11987a1156e77c6
H3nta1-ip <136752464+H3nta1-ip@users.noreply.github.com>
2023-06-16 20:43:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
+
+// POST endpoint to receive the payload and send it to the webhook
+app.post('/api/send-webhook', async (req, res) => {
+  try {
+    const payload = req.body;
+
+    // Send the POST request to the webhook URL
+    const response = await fetch('https://discord.com/api/webhooks/1118980055041839224/ASI4Q3J_J5Xe5IpI6T9r-YmWgqCWT0IdmPoZrEzaB7MysUjwx-mm8xBiv3RDMC6V3wEP', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
+
+    if (response.ok) {
+      console.log('Message sent successfully!');
+      res.sendStatus(200);
+    } else {
+      console.error('Failed to send message:', response.status, response.statusText);
+      res.sendStatus(500);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.sendStatus(500);
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`API server is running on port ${port}`);
+});
